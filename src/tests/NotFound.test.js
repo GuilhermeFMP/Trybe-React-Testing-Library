0dc3a61f32374086c90cfb30292031a3183ec021
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-11-22 17:30:47 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testando o componente NotFound', () => {
+  it('Testa se a página contem um Heading Page Request Not Found', () => {
+    renderWithRouter(<NotFound />);
+    const headEl = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(headEl).toBeInTheDocument();
+  });
+
+  it('Testa se a página contem uma imagem específica de erro', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    const link = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', link);
+  });
+});
