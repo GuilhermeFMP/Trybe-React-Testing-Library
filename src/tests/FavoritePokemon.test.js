7fe256477e382bc1bcb4bf5b73319625f55594d1
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-11-22 17:18:51 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Testando o componente FavoritePokemon', () => {
+  it('Testa se é exibido a mensagem No Favorite Pokemon Found', () => {
+    const { history } = renderWithRouter(<FavoritePokemon />);
+    act(() => {
+      history.push('./favorites');
+    });
+    const notPokemon = screen.getByText(/no favorite pokémon found/i);
+
+    expect(notPokemon).toBeInTheDocument();
+  });
+});
